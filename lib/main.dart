b33a522c86a125d75d3f989ed559f98f0e7c3ca4
Fredd <fredd85565@gmail.com>
2023-09-09 00:27:00 -0400

    fix: :heavy_plus_sign: Bloc and Flutter_Bloc
    se importaron las librerias, Bloc y Flutter_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/pages/home_page.dart';
 // import 'package:bloc/pages/home_page_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_case_bloc/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
